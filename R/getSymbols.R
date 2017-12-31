c5745a5e6b1a5a912f2261b534c0aead2eb8834d
phamdinhkhanh <phamdinhkhanh.tkt53.neu@gmail.com>
2017-12-31 20:34:00 +0700

    add get data from CP68

--- R/getSymbols.R
+++ R/getSymbols.R
@@ -6,14 +6,23 @@
 #' @param from ngay bat dau dinh dang yyyy-mm-dd
 #' @param to ngay ket thuc dinh dang yyyy-mm-dd
 #' @return mot data frame chua gia chung khoan va khoi luong
-#' @export 
+#' @export getSymbols
 #' @import rvest
 #' @import xml2
 #' @import httr
 #' @import stringr
 #' @import magrittr
 #' @import dplyr
-getSymbols <- function(symbol, from, to){
+
+getSymbols <- function(symbol,from,to,src="VND"){
+  if(src=="VND"){
+    invisible(getSymbolVND(symbol,from,to))
+  } else if(src=="CP68"){
+    invisible(getSymbolCP68(symbol,from,to))
+  }
+}
+
+getSymbolVND <- function(symbol, from, to){
   url <- "https://www.vndirect.com.vn/portal/thong-ke-thi-truong-chung-khoan/lich-su-gia.shtml"
   #lay page cuoi cung
   lastPage <- getLastPage(url,symbol,from,to)
@@ -49,12 +58,12 @@ getSymbols <- function(symbol, from, to){
   symbolData <- data.frame(symbolData,row.names = symbolData[,1])
   symbolData[,1]<- as.Date(symbolData[,1],"%Y-%m-%d")
   symbolData[,2:11] <- apply(symbolData[,2:11],2,as.numeric)
-  myexport(symbolData)
+  #myexport(symbolData)
   #doi ten cho bang
-  assign(symbol,symbolData,envir = .GlobalEnv)
-  rm(list = "symbolData",envir = .GlobalEnv)
-  return(list(symbolData,
-              call = match.call()))
+  #assign(symbol,symbolData,envir = .GlobalEnv)
+  #rm(list = "symbolData",envir = .GlobalEnv)
+  cat(paste0("#",symbol," from ",from, " to ",to," already cloned "))
+  invisible(symbolData)
 }
 
 getLastPage <- function (url,symbol,from,to){
@@ -88,6 +97,91 @@ dateChar <- function(dateTime){
   dateTime %<>%  as.Date() %>% format(.,format = "%d/%m/%Y")
   return(dateTime)
 }
+
+
+
+################################## get data CP68 ###############################################
+
+getSymbolCP68 <- function (symbol,from,to){
+  url<-"http://www.cophieu68.vn/historyprice.php"
+  #lay cac trang cuoi cung
+  fd<-list(
+    id = symbol
+  )
+
+  resp <- POST(url,body=fd,endcode="form")
+  resp %>% read_html() %>% html_nodes(xpath='//*[@id="navigator"]/li[7]/a') %>%
+    html_attrs() -> tmp
+  positionString <- str_locate(tmp[1],"[0-9]") %>% as.data.frame() %$% start
+  lastPage <- as.numeric(str_sub(unlist(tmp[1]),positionString,positionString+1))
+
+  #khoi tao dataframe
+  symbolData <- data.frame(
+    DATE = character(),
+    REF.PRICE = numeric(),
+    CHANGE.PERCENT1 = numeric(),
+    CHANGE.PERCENT2 = numeric(),
+    CLOSE = numeric(),
+    MATCH.VOLUME = numeric(),
+    OPEN = numeric(),
+    HIGH = numeric(),
+    LOW = numeric(),
+    RECONCILE.VOLUME = numeric(),
+    FOREIGN.BUY = numeric(),
+    FOREIGN.SALE = numeric(),
+    VALUE = numeric()
+  )
+
+  #tao vong lap lay du lieu
+  for(i in 1:lastPage){
+    #tao form-data
+    fd<-list(
+      currentPage = i,
+      id = symbol
+    )
+
+    resp <- POST(url,body=fd,endcode="form")
+
+
+    resp %>% read_html() %>% html_nodes(xpath='//*[@id="content"]/table') %>%
+      html_table() %>% as.data.frame() -> tmp
+    tmp <- tmp[-1,2:14]
+    tmp[,1] <- as.Date(convertDate(tmp[,1]),format = "%Y-%m-%d")
+    #check dieu kien de continue
+    if(min(tmp[,1]) > to){next}
+    #check dieu kien de break
+    if(max(tmp[,1]) < from){break}
+    tmp[,c(6,10:12)] <- apply(tmp[,c(6,10:12)],2,subComma)
+    tmp[,4] <- convertPercent(tmp[,4])
+    tmp[,c(2:3,5:13)] <- apply(tmp[,c(2:3,5:13)],2,as.numeric)
+
+    colnames(tmp)<-c("DATE","REF.PRICE","CHANGE.PERCENT1","CHANGE.PERCENT2","CLOSE","MATCH.VOLUME","OPEN","HIGH","LOW","RECONCILE.VOLUME","FOREIGN.BUY","FOREIGN.SALE","VALUE")
+    symbolData <- rbind(symbolData,tmp)
+  }
+
+  symbolData <- data.frame(symbolData,row.names = symbolData[,1])
+  colnames(symbolData)<-c("DATE","REF.PRICE","CHANGE.PERCENT1","CHANGE.PERCENT2","CLOSE","MATCH.VOLUME","OPEN","HIGH","LOW","RECONCILE.VOLUME","FOREIGN.BUY","FOREIGN.SALE","VALUE")
+  symbolData <- symbolData[,c(1,3:4,7:9,5,2,6,10:13)]
+  cat(paste0("#",symbol," from ",from, " to ",to," already cloned "))
+  invisible(subset(symbolData,and(DATE >= from, DATE <= to)))
+}
+
+
+#ham convertDate
+convertDate <- function(dt){
+  paste0(str_sub(dt,7,10),"-",str_sub(dt,4,5),"-",str_sub(dt,1,2))
+}
+
+#ham thay the dau comma
+subComma <- function(v) {
+  str_replace_all(v,",","")
+}
+
+#chuyen percent dang text sang numeric
+convertPercent <- function(v){
+  str_replace_all(v,"%","") %>% as.numeric()*0.01
+}
+
 
 
 
