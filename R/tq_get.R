5989a5c6ccccf3c24f6b12caf8316ce6144fc54a
phamdinhkhanh <phamdinhkhanh.tkt53.neu@gmail.com>
2018-05-12 00:06:51 +0700

    ..

--- R/tq_get.R
+++ R/tq_get.R
@@ -108,7 +108,7 @@ tq_get_single <-
     switch(return.type,
       tibble = tq_get_single_tibble(symbol,from,to,src,minimal),
       xts = tq_get_single_tibble(symbols, from, to, src, minimal) %>%
-        xts(symbol,order.by=.$date)
+        xts(order.by=.$date)
       )
   }
 
