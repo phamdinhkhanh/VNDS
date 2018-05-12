f49ee47d9fbb46fa30402d27c65d91e721dc090d
phamdinhkhanh <phamdinhkhanh.tkt53.neu@gmail.com>
2018-05-12 16:27:52 +0700

    update from to when NA tq_get

--- R/tq_get.R
+++ R/tq_get.R
@@ -127,8 +127,8 @@ tq_get_single <-
 # Get 1 symbol df-----------------------------------------------------------------------------
 tq_get_single_tibble <-
   function(symbol,
-           from,
-           to,
+           from=NA,
+           to=NA,
            src="VND",
            minimal = TRUE,
            ...){
@@ -140,9 +140,8 @@ tq_get_single_tibble <-
                 "close",
                 "volume",
                 "adjusted")
-
-    if (is.na(from)){from <- '1900-01-01'}
     if (is.na(to)){to <- as.character(Sys.Date()-1)}
+    if (is.na(from)){from <- as.Date(to-365)}
     
     if(src=="VND"){
       extractData <- tq_get_vnd(symbol,from,to)
